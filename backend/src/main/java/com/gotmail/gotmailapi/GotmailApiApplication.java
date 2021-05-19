da0f580732cd5fc4a58efae48b70503599dbaf2e
Samuel Clinton <contato@samvkn.dev>
2021-05-19 13:33:12 -0300

    Backend project created

---
+++ backend/src/main/java/com/gotmail/gotmailapi/GotmailApiApplication.java
@@ -0,0 +1,13 @@
+package com.gotmail.gotmailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotmailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotmailApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gotmail/gotmailapi/GotmailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotmail.gotmailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotmailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
